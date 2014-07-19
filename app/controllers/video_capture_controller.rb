ca80c7f884b2062d23b167d75c0c13e0b8dc1672
surendra <surendranath@elitmus.com>
2014-07-20 03:29:47 +0530

    successfully able to upload and save images

--- app/controllers/video_capture_controller.rb
+++ app/controllers/video_capture_controller.rb
@@ -1,6 +1,14 @@
 class VideoCaptureController < ApplicationController
 
 	def index
+		unless params[:img].blank?
+			# puts "#{params[:img].inspect}"
+			  data = params[:img]
+			  data_index = data.index('base64') + 7
+			  filedata = data.slice(data_index, data.length)
+			File.open("a.png", "wb"){ |file| file.write(Base64.decode64(filedata)) }
+		end
+		# render js: []
 
 	end
 
