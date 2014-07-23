7bdcdb14c824ccfd1ff2bce223c70f4ed8f5b977
surendra <surendranath@elitmus.com>
2014-07-23 22:17:44 +0530

    not working version

--- app/controllers/video_capture_controller.rb
+++ app/controllers/video_capture_controller.rb
@@ -3,10 +3,11 @@ class VideoCaptureController < ApplicationController
 	def index
 		unless params[:img].blank?
 			# puts "#{params[:img].inspect}"
+    		  filename = 'img/' + Time.now.to_s + '.png'
 			  data = params[:img]
 			  data_index = data.index('base64') + 7
 			  filedata = data.slice(data_index, data.length)
-			File.open("a.png", "wb"){ |file| file.write(Base64.decode64(filedata)) }
+			  File.open(filename, "wb"){ |file| file.write(Base64.decode64(filedata)) }
 		end
 		# render js: []
 
